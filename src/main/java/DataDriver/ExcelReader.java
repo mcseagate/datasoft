6a2b0240bef31cca2b1113dbe5bb1c26c2ab6cbb
Michael Godfrey <mcseagate@gmail.com>
2019-01-07 17:21:59 -0800

    New Files

---
+++ src/main/java/DataDriver/ExcelReader.java
@@ -0,0 +1,38 @@
+package DataDriver;
+
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import java.io.FileInputStream;
+
+
+
+public class ExcelReader {
+    XSSFWorkbook excelWBook;
+    XSSFSheet excelSheet;
+    XSSFCell excelCell;
+    String path = "Path Name Should Be Affixed Here";
+    String sheetName = "Sheet1";
+    FileInputStream excelFile;
+
+
+
+    public  ExcelReader(){
+
+
+        try{
+
+            excelFile = new FileInputStream(path);
+            excelWBook = new XSSFWorkbook(excelFile);
+            excelSheet = excelWBook.getSheet(sheetName);
+            excelCell = excelSheet.getRow(0).getCell(0);
+            String cellData = excelCell.getStringCellValue();
+            System.out.println("Cell Data" + cellData);
+        }
+
+        catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
+}
